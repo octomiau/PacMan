ff3c4e5adf2d3e3ac5609600665845dc1960cd1e
Diana Aviles <120342520+octomiau@users.noreply.github.com>
2023-05-09 21:02:14 -0600

    Add files via upload
    **CSS**
    -Created a CSS grid layout with three columns and three rows.
    -Made the buttons inside aside change size proportionally to the viewport.
    -Used background-size: cover to make the custom button images fit the buttons without getting trimmed.
    -Two new buttons were created: refresh & pause.
    
    **HTML**
    -In the HTML portion of the project, I assigned classes to the buttons and linked them to the corresponding functions within the CSS.
    
    **JS**
    -New items are created when added to the DOM and can move accordingly.
    -The functions where separated for them to take arrays as arguments in order to upload 2 types of images to the DOM (pacmans & ghosts) as well as for the image to change when hitting a boundary.
    -The function "getArticleDimensions()" was created in order for the animation change direction when hitting the article borders instead of the viewports and to maintain those borders even if the window resizes.
    -The function "stopAndFreezeAnimation()" was created to have a "pause" button. 
    - The variable "isRunnig" was created for the "stopAndFreezeAnimation()" function to be able to reset the animation where the items where located.

---
+++ code_ghostsandpacs.js
@@ -0,0 +1,161 @@
+var pos = 0;
+const pacArray = [
+  ['./ghost1.png', './ghost2.png'],
+  ['./PacMan1.png', './PacMan2.png'],
+];
+var direction = 0;
+const pacMan = [];
+let articleWidth;
+let articleHeight;
+let timeoutId;
+
+
+function getArticleDimensions() {
+    const articleElement = document.querySelector("article");
+    articleWidth = articleElement.clientWidth;
+    articleHeight = articleElement.clientHeight;
+  }
+  
+
+function setToRandom(scale) {
+    return {
+      x: Math.random() * scale,
+      y: Math.random() * scale,
+    };
+  }
+  
+
+  // Call setToRandom with a larger scale value when creating new Pac-Man objects
+  let position = setToRandom(articleWidth - 100); // Subtract 100 to account for the Pac-Man image width
+  
+  // Call this function at the end of the makePac function to update the Pac-Man position
+  function updatePacPosition(pac) {
+    pac.position.x = setToRandom(articleWidth - 100).x;
+    pac.position.y = setToRandom(articleHeight - 100).y;
+    pac.newimg.style.left = pac.position.x + "px";
+    pac.newimg.style.top = pac.position.y + "px";
+  }
+  
+// Factory to make a PacMan
+function makePac(imageSet) {
+    // Update the article dimensions when creating a new Pac-Man
+    getArticleDimensions(); 
+    // returns an object with values scaled {x: 33, y: 21}
+    let velocity = setToRandom(5);
+    let position = setToRandom(50);
+    // Add image to div id = game
+    let game = document.getElementById('game');
+    let newimg = document.createElement('img');
+    newimg.style.position = 'absolute';
+    newimg.src = pacArray[imageSet][0];
+    newimg.style.position = 'absolute';
+    newimg.src = 'ghost1.png';
+    newimg.width = 30;
+    newimg.style.left = position.x  + "px";
+    newimg.style.top = position.y + "px";
+    game.appendChild(newimg);
+    // new style of creating an object
+    let pac = {
+        position,
+        velocity,
+        newimg,
+        imageSet,
+  };
+
+  pacMan.push(pac); // add the new PacMan object to the pacMan array
+
+  updatePacPosition(pac);
+  return pac;
+}
+
+
+function update() {
+    // Call getArticleDimensions here
+    getArticleDimensions(); 
+    // Loop over pacmen array and move each one and move image in DOM
+    pacMan.forEach((item) => {
+        checkCollisions(item);
+        item.position.x += item.velocity.x;
+        item.position.y += item.velocity.y;
+      
+        item.newimg.style.left = item.position.x + "px";
+        item.newimg.style.top = item.position.y + "px";
+    });
+    setTimeout(update, 1000 / 60); // Update at 60 FPS
+}
+
+
+function stopAndFreezeAnimation() {
+    // Stop PacMan movement by clearing the timeout
+    clearTimeout(timeoutId);
+    // Pause PacMan animation by setting velocity to 0
+    pacMan.forEach((item) => {
+        item.velocity = { x: 0, y: 0 }; // set velocity to 0 to freeze movement
+    });
+}
+
+function checkCollisions(item) {
+    const alternativeImages = pacArray[item.imageSet];
+    
+    if (
+      item.position.x + item.velocity.x + item.newimg.width > articleWidth ||
+      item.position.x + item.velocity.x < 0
+    ) {
+      item.velocity.x = -item.velocity.x;
+      item.newimg.src = alternativeImages[0];
+    }
+    if (
+      item.position.y + item.velocity.y + item.newimg.height > articleHeight ||
+      item.position.y + item.velocity.y < 0
+    ) {
+      item.velocity.y = -item.velocity.y;
+      item.newimg.src = alternativeImages[1];
+    }
+  }
+  
+  function makePac(imageSet) {
+    // Update the article dimensions when creating a new Pac-Man
+    getArticleDimensions();
+    // returns an object with values scaled {x: 33, y: 21}
+    let velocity = setToRandom(5);
+    let position = setToRandom(50);
+    // Add image to div id = game
+    let game = document.getElementById("game");
+    let newimg = document.createElement("img");
+    newimg.style.position = "absolute";
+    newimg.src = pacArray[imageSet][0]; // Set the initial image based on the imageSet
+    newimg.width = 30;
+    newimg.style.left = position.x + "px";
+    newimg.style.top = position.y + "px";
+    game.appendChild(newimg);
+    // new style of creating an object
+    let pac = {
+      position,
+      velocity,
+      newimg,
+      imageSet, 
+    };
+  
+    pacMan.push(pac); // add the new PacMan object to the pacMan array
+  
+    updatePacPosition(pac);
+    return pac;
+  }
+  
+  function makeOne(imageSet) {
+    pacMan.push(makePac(imageSet));
+  }
+  
+
+
+  
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = { checkCollisions, update, pacMan };
+}
+
+
+// Call the getArticleDimensions function when the DOM is ready
+document.addEventListener("DOMContentLoaded", getArticleDimensions);
+// Update the article dimensions when the window is resized
+window.addEventListener("resize", getArticleDimensions);
